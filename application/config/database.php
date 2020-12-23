b8a4ed0f79d57412fe6f341875554484b2a8ef6b
Llowlyn Smith <38695711+samurailos@users.noreply.github.com>
2020-12-23 07:51:40 -0500

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -73,7 +73,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
-$db['default'] = array(
+/*$db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
 	'username' => 'root',
@@ -92,5 +92,26 @@ $db['default'] = array(
 	'compress' => FALSE,
 	'stricton' => FALSE,
 	'failover' => array(),
-	'save_queries' => TRUE
+	'save_queries' => TRUE*/
+
+	$db['default'] = array(
+		'dsn'	=> '',
+		'hostname' => 'remotemysql.com',
+		'username' => 'OOpAJs8VPh',
+		'password' => 'A8xV3UYbOB',
+		'database' => 'OOpAJs8VPh',
+		'dbdriver' => 'mysqli',
+		'dbprefix' => '',
+		'pconnect' => FALSE,
+		'db_debug' => (ENVIRONMENT !== 'production'),
+		'cache_on' => FALSE,
+		'cachedir' => '',
+		'char_set' => 'utf8',
+		'dbcollat' => 'utf8_general_ci',
+		'swap_pre' => '',
+		'encrypt' => FALSE,
+		'compress' => FALSE,
+		'stricton' => FALSE,
+		'failover' => array(),
+		'save_queries' => TRUE
 );
